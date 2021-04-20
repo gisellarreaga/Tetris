c9e17e887af5aef92f4f26de51169a9dfd57e88f
Gisell Arreaga <jackeline_gosell@hotmail.com>
2021-04-19 23:34:04 -0700

    Declaración de variables y se agregaron los recursos multimedia del proyecto.

--- Tetris/App.xaml.cs
+++ Tetris/App.xaml.cs
@@ -5,13 +5,31 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace Tetris
 {
     /// <summary>
-    /// Lógica de interacción para App.xaml
+    /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        //Declaracion de Variables
+        //Lista para agregar 2 sonidos .wav
+        List<System.Media.SoundPlayer> soundList = new List<System.Media.SoundPlayer>();
+        //DispatcherTime
+        DispatcherTimer timer;
+        //Figuras Random
+        Random shapeRandom;
+
+
+        //Reproducir el sonido. Index = 0 va reproducir collided.wav y Index = 1 Reproducri Delete Line
+        private void playSound(int index)
+        {
+            soundList[index].Play();
+        }
     }
+
+
+
 }
--- Tetris/MainWindow.xaml.cs
+++ Tetris/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Tetris
 {
@@ -20,14 +21,62 @@ namespace Tetris
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Declaracion de Variables
+        //Lista para agregar 2 sonidos .wav
+        List<System.Media.SoundPlayer> soundList = new List<System.Media.SoundPlayer>();
+        //DispatcherTime
+        DispatcherTimer timer;
+        //Figuras Random
+        Random shapeRandom;
+
+        //Colores de las figuras(Terominos)
+        private static Color O_TetrominoColor = Colors.GreenYellow;
+        private static Color I_TetrominoColor = Colors.Red;
+        private static Color T_TetrominoColor = Colors.Gold;
+        private static Color S_TetrominoColor = Colors.Violet;
+        private static Color Z_TetrominoColor = Colors.DeepSkyBlue;
+        private static Color J_TetrominoColor = Colors.Cyan;
+        private static Color L_TetrominoColor = Colors.LightSeaGreen;
+
+        //Listas de la posicion actual de los Tetrominos
+        //Row
+        List<int> currentTetrominoRow = null;
+
+        //Columna
+        List<int> currentTetrominoColumn = null;
+
+        //Variable para nivel
+        private int gameLevel = 1;
+
+        //Variable para el score
+        private int gameScore = 0;
+
+        //Variable para contar la velocidad
+        private double gameSpeedCounter = 0;
+
+        //Variable para gameover
+        private bool isGameOver = false;
+
+        //Variable para verificar si choco con el limite
+        private bool bottomCollided = false;
+        private bool leftCollided = false;
+        private bool rightCollided = false;
+
+        //Variable para saber si roto
+        private bool isRotated = false;
+        private int rotation = 0;
+
+
+
+        //Reproducir el sonido. Index = 0 va reproducir collided.wav y Index = 1 Reproducri Delete Line
+        private void playSound(int index)
+        {
+            soundList[index].Play();
+        }
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
